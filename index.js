093d0815b76f316fedb199dc84f44854b30fc3ca
urnisha19 <urnisha09@gmail.com>
2021-06-26 21:25:31 +0600

    heroku update

--- index.js
+++ index.js
@@ -19,60 +19,60 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
-  const blogsCollection = client.db("Blogs").collection("blogsCollection");
+    const blogsCollection = client.db("Blogs").collection("blogsCollection");
 
-  // <----------Get Methods---------->
-  // check server connection
-  app.get('/', (req, res) => {
-    res.send('server working');
-  });
-  console.log("DB connected");
-
-  //get all blogs
-  app.get('/blogs', (req, res) => {
-    blogsCollection.find({})
-      .toArray((err, documents) => {
-        res.send(documents);
-      })
-  });
-
-  // get blog by object id
-  app.get('/blog/:id', (req, res) => {
-    blogsCollection.find({_id: ObjectId(req.params.id)})
-    .toArray((err, documents) => {
-        res.send(documents[0]);
-    })
-  });
-
-  // <----------Delete---------->
-  // delete blog by object id
-  app.delete('/admin/deleteBlog/:id', (req, res)=>{
-    const id = ObjectId(req.params.id); 
-    blogsCollection.findOneAndDelete({_id: id})
-    .then(result => {
-        res.send(result.deletedCount > 0);
+    // <----------Get Methods---------->
+    // check server connection
+    app.get('/', (req, res) => {
+        res.send('server working');
     });
-  });
-   
+    console.log("DB connected");
 
-  // <----------Post Methods---------->
-  //Add Blog
-  app.post('/admin/addBlog', (req, res) => {
-    const title = req.body.title;
-    const content = req.body.content;
-    const file = req.files.file;
-    const newImg = file.data;
-    const encImg = newImg.toString('base64');
-    var image = {
-      contentType: file.mimType,
-      size: file.size,
-      img: Buffer.from(encImg, 'base64')
-    };
-    blogsCollection.insertOne({ title, content, image })
-      .then(result => {
-        res.send(result.insertedCount > 0);
-      })
-  });
+    //get all blogs
+    app.get('/blogs', (req, res) => {
+        blogsCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    });
+
+    // get blog by object id
+    app.get('/blog/:id', (req, res) => {
+        blogsCollection.find({ _id: ObjectId(req.params.id) })
+            .toArray((err, documents) => {
+                res.send(documents[0]);
+            })
+    });
+
+    // <----------Delete---------->
+    // delete blog by object id
+    app.delete('/admin/deleteBlog/:id', (req, res) => {
+        const id = ObjectId(req.params.id);
+        blogsCollection.findOneAndDelete({ _id: id })
+            .then(result => {
+                res.send(result.deletedCount > 0);
+            });
+    });
+
+
+    // <----------Post Methods---------->
+    //Add Blog
+    app.post('/admin/addBlog', (req, res) => {
+        const title = req.body.title;
+        const content = req.body.content;
+        const file = req.files.file;
+        const newImg = file.data;
+        const encImg = newImg.toString('base64');
+        var image = {
+            contentType: file.mimType,
+            size: file.size,
+            img: Buffer.from(encImg, 'base64')
+        };
+        blogsCollection.insertOne({ title, content, image })
+            .then(result => {
+                res.send(result.insertedCount > 0);
+            })
+    });
 
 });
 
